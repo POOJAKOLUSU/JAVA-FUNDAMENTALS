3798961db9ce46c6d8daa448c37fee5a15fb9eee
POOJAKOLUSU <kolusupooja22@gmail.com>
2026-09-16 08:51:30 -0400

    datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,25 @@
+public class Typo{
+    public static void main(String[]args){
+        //program to demonstrate primitive data types
+         byte b=126;
+        short s=32456;
+        int num=2345673;
+        long l=123456789l;
+        float marks=7.66f;
+        double cgpa=7.66;
+        char c='k';
+        boolean isfishy=false;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(l);
+        System.out.println(marks);
+        System.out.println(cgpa);
+        System.out.println(c);
+        System.out.println(isfishy);
+        
+
+        
+        
+    }
+}
