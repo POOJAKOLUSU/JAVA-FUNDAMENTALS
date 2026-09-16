8ad82c78911a4bc2c64fc60602f2ea6fe400c35a
POOJAKOLUSU <kolusupooja22@gmail.com>
2026-09-16 03:15:18 -0400

    JavaPrintVsPrintln.java

---
+++ JavaPrintVsPrintln.java
@@ -0,0 +1,18 @@
+
+
+public class Zia{
+    public static void main(String[] args){
+        System.out.println(16+14);
+        System.out.println(8-4);
+        System.out.print("pooja");
+        System.out.print("kolusu");
+    }
+}
+
+
+output
+  
+  30
+  4
+  poojakolusu
+  
